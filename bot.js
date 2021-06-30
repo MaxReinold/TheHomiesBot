01cea6aaf13ec0c6dda64c1135f91f3c7bd560f1
StealthSov <50644368+StealthSov@users.noreply.github.com>
2021-06-29 22:33:13 -0400

    Added !summon

--- bot.js
+++ bot.js
@@ -4,24 +4,74 @@ const config = require('./config.json');
 const credentials = require('./credentials.json');
 let global_commands = {};
 let server_commands = {};
-let default_prefix = "!";
+let prefix = "!";
+let summon_overrides = {
+  259701911320657920:true,
+  181185781094809610:true
+}
+let summon_limit = 10;
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  createGlobalCommand("test", async interaction => {
-    console.log(`test ${interaction}`);
+  createGlobalCommand("summon", context => {
+    // console.log(context.mentions.users.first());
+    if(context.args[0] && (context.member.hasPermission('ADMINISTRATOR') || summon_overrides[context.member.id])) {
+      let member = context.mentions.members.first();
+      if(member) {
+        let temp = context.mentions.members.first().voice.channel;
+        let goto = context.guild.channels.cache.filter(c => c.type=="voice").filter(c => !c.members.first()).random();
+        let count = 4;
+        if(parseInt(context.args[1])){
+          count = parseInt(context.args[1]);
+        }
+        if (count > summon_limit) {
+          context.reply(`The current summon limit is ${summon_limit} and your input has been capped`);
+          count = summon_limit;
+        }
+        if(temp) {
+          for(let i = 0; i< count; i++){
+            member.voice.setChannel(goto);
+            member.voice.setChannel(temp);
+          }
+        }
+      }
+    }
   });
 });
 
-client.on('interaction', async interaction => {
-    if (!interaction.isCommand()) return;
-    if(global_commands[interaction.commandName] != null) {
-        await global_commands[interaction.commandName](interaction);
-    }
+client.on('message', msg => {
+  // ALMOND DINK DONK
+  if(msg.content === "dink" && (msg.author.id == 251132701917184000)){
+    msg.reply("donk");
+  }
+  //testing section
+  if(msg.author.id == 251132701917184000) {
+    
+  }
+  validateCommand(msg);
 })
 
-client.login(credentials.token);
+function validateCommand(msg){
+  message = msg.content
+  if(message.substring(0,prefix.length) == prefix) {
+    var split = message.split(" ");
+    let command = split[0].substring(prefix.length, split[0].length)
+    console.log(`Parsed command: ${command}`)
+    if(global_commands[command] != null) {
+      ctx = msg;
+      ctx.args = [];
+      for(let i = 1; i < split.length; i++){
+        ctx.args.push(split[i]);
+      }
+      global_commands[command](ctx);
+    } else if (server_commands[msg.guild.id][command] != null){
+      server_commands[msg.guild.id][command](ctx);
+    }
+  }
+}
 
 function createGlobalCommand(command, callback) {
     global_commands[command]  = callback;
 }
+
+client.login(credentials.token);
